4d2d375c3af06574941cac2ab6520228a018fd85
madasilva1 <madasilva195@gmail.com>
2021-02-14 11:45:27 -0500

    resset button solved

--- src/GuessTest.java
+++ src/GuessTest.java
@@ -1,6 +1,5 @@
-import javax.swing.*;
+
 import java.awt.*;
-import java.awt.BorderLayout;
 import java.awt.event.*;
 import java.util.ArrayList;
 
@@ -12,12 +11,13 @@ public class GuessTest extends Frame implements ActionListener {
     private final TextField tfentered;
     private final TextField tfStill;
     private final TextField tfNumber;
-   // JLabel tfNumbe = new JLabel("sample text");
-   // Label lblTitle;
+
     //declare Buttons
     private final Button btnNumber;
+    private final Button resset;
+
+    //track entry numbers
     private int  count = 0;
-    //declare a Label
 
     //declare array to show up entered numbers
     ArrayList<Integer> element = new ArrayList<>();
@@ -32,31 +32,20 @@ public class GuessTest extends Frame implements ActionListener {
                 dispose();
             }
         });
-         setLayout(new FlowLayout());// "super" Frame, which is a Container, sets its layout to FlowLayout to arrange
-        // the components from left-to-right, and flow to next row from top-to-bottom.
 // Declare a Label component
-        Label  LbTitle = new Label("you have random");
-        add(LbTitle);
-        LbTitle.setVisible(true);
-        //LbTitle.setBounds(200,70,200,100);
-        //LbTitle.setFont(new Font(LbTitle.getName(), Font.PLAIN, 18));
+       // Label  LbTitle = new Label("you have random");
+       // add(LbTitle);
+        //setSize(400, 300);// "super" Frame sets its initial window size
+       // "super" Frame shows
 
-
-       //Label lblTitle  = new Label("you have choice it");
-        //add(lblTitle);
-
-       //  lblTitle.setVisible(true);
-       // lblTitle.setFont(new Font(lblTitle.getName(), Font.PLAIN, 18));
-
-
-
+        // Declare and add Text Fields
         tfResolted = new TextField("" + "", 10);
         tfResolted.setBounds(50, 50, 150, 20);
         tfResolted.setEditable(true);
         add(tfResolted);
 
         tfentered = new TextField("" + "", 10);
-        tfentered.setBounds(50, 150, 210, 20);
+        tfentered.setBounds(50, 150, 255, 20);
         tfentered.setEditable(false);
         add(tfentered);
 
@@ -69,6 +58,7 @@ public class GuessTest extends Frame implements ActionListener {
         tfNumber.setBounds(50, 100, 150, 20);
         tfNumber.setEditable(false);// set to read-only
         add(tfNumber);// "super" Frame container adds Button component
+
 // Declare a Button component
         btnNumber = new Button("Press");
         btnNumber.setBounds(220, 45, 50, 30);
@@ -79,11 +69,13 @@ public class GuessTest extends Frame implements ActionListener {
         //   an ActionEvent handler called actionPerformed().
         // Clicking "btnCount" invokes actionPerformed().
 
-//setTitle("guess counter,randomly number from 1 to 100");// "super" Frame sets its title
-        setSize(400, 300);// "super" Frame sets its initial window size
-        setLayout(null);
-        setVisible(true);// "super" Frame shows
-
+        //initialise button resset
+      resset = new Button("Resset");
+      resset.setBounds(220,95,60,30);
+      add(resset);
+      resset.addActionListener(this);
+      setLayout(null);
+      setVisible(true);
     }
 
     // The entry main() method
@@ -96,13 +88,18 @@ public class GuessTest extends Frame implements ActionListener {
         mylabele.setFont(new Font(mylabele.getName(), Font.PLAIN, 14));
         mylabele.setSize(500,500);
         mylabele.setVisible(true);
+       // mylabele.add(mylabele.resset);
     }
 
     // ActionEvent handler - Called back upon button-click.
     @Override
     public void actionPerformed(ActionEvent evt) {
 
+                              if(evt.getSource() == resset){
+                                  clearfields();
+                                  element.clear();
 
+                              }
                             String tfr = tfResolted.getText();
                             int a = Integer.parseInt(tfr);
 
@@ -111,22 +108,31 @@ public class GuessTest extends Frame implements ActionListener {
                             String str2 = "Smaller than that";
                             String str3 = "You lost";
                             if (evt.getSource() == btnNumber && randomNumber > a && count < 10) {
-                                tfNumber.setText(str1);
+                                tfNumber.setText(""+str1);
 
                             } else if (evt.getSource() == btnNumber && randomNumber < a && count < 10) {
-                                tfNumber.setText(str2);
+                                tfNumber.setText(""+str2);
 
                             } else if (evt.getSource() == btnNumber && randomNumber == a && count < 10) {
-                                tfNumber.setText(str0);
+                                tfNumber.setText(""+str0);
 
                             }
                             else{
-                                tfStill.setText(str3);
-                               
+                                tfStill.setText(""+str3);
+
                             }
-                            element.add(a) ;
+
+                            element.add(a);
                             tfentered.setText(element.toString().replace("[", "").replace("]", ""));
 
             count++;                // Display the text and entered numbers  on the TextField tfentered
         }
-    }
+        private void clearfields( ){
+           // this.remove(element.this);
+             tfResolted.setText("");
+             tfentered.setText("");
+             tfStill.setText("");
+             tfNumber.setText("");
+         }
+        }
+
